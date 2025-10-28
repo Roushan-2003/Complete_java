d4a46cb259f20bc407511178ba3dfab412766924
Roushan-2003 <roushanmishra909@gmail.com>
2025-10-28 20:25:04 +0530

    Updated SumOfOddNumbers logic

---
+++ java/SumOfOddNumbers.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class SumOfOddNumbers{
+	
+	// function to calculate all the odd numbers from 1 to n;
+	
+	static int sumOfOdds(int n) {
+		int sum = 0;
+		for(int i=0; i<=n ; i++){
+			if(i % 2 != 0) {//check if the number is odd
+				sum += i;
+			}
+		}
+		return sum;
+	}
+	
+	public static void main(String args[]){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//take input from user
+		System.out.print("Enter a number: ");
+		int n = sc.nextInt();
+		
+		int result =  sumOfOdds(n);
+		System.out.println("The sum of odd numbers from 1 to "+ n +" is : "+ result);
+	}
+}
