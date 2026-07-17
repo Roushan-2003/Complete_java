c5b2084833aeb549849de7d19754cb902311048a
Roushan-2003 <roushanmishra909@gmail.com>
2026-07-17 09:09:24 +0530

    Update project

--- java/PersonalDetails.java
+++
@@ -1,7 +0,0 @@
-public class PersonalDetails{
-	public static void main(String[] args) {
-	System.out.println("Name: Roushan Mishra");
-	System.out.println("Age: 23");
-	System.out.println("College: Compuetr science Honors");
-	}
-}
--- java/roshan.java
+++
@@ -1,5 +0,0 @@
-public class roshan {
-    public static void main(String[] args) {
-        System.out.println("Hello, Roushan");
-    }
-}
